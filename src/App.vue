a9898b4b6eb96ab08f827225c4589b327a18f454
2155507151 <2155507151@qq.com>
2023-07-19 14:34:16 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <button @click="age++">年龄+1</button>
+  {{name}}
+  {{word.i}}
+</template>
+
+<script>
+
+import { watch, reactive,toRefs,toRef } from "vue";
+export default {
+  name: 'App',
+  setup() {
+    const person = reactive({
+      name: 'chen',
+      age: 23,
+      word: {
+        i: 'hello',
+        j: 'world'
+      }
+    });
+    watch(person, (n, o) => {
+      console.log(toRefs(person));
+    })
+
+    return {
+      ...toRefs(person)
+    }
+
+  }
+
+
+}
+</script>
